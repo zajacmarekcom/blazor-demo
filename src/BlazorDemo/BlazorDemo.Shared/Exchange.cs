44b0e2283ff9198cf5976c82d89b498d04b1a625
Marek Zajac <contact@zajacmarek.com>
2020-09-02 23:41:31 +0200

    Install AntDesign and add test layout

---
+++ src/BlazorDemo/BlazorDemo.Shared/Exchange.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorDemo.Shared
+{
+    public class Exchange
+    {
+        public string name { get; set; }
+        public string acronym { get; set; }
+        public string mic { get; set; }
+        public string country_code { get; set; }
+        public string city { get; set; }
+    }
+}
---
+++ src/BlazorDemo/BlazorDemo.Shared/ExchangeResponse.cs
@@ -0,0 +1,7 @@
+﻿using BlazorDemo.Shared;
+using System.Collections.Generic;
+
+public class ExchangeResponse
+{
+    public IEnumerable<Exchange> data { get; set; }
+}
